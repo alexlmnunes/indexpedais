1a666a266a81042bcd84f56d27d80c85b7fa3061
leansteelbean <alexlmnunes03@gmail.com>
2026-05-25 00:02:00 -0300

    main.go pronto, ui começado

---
+++ api/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "./src/ui"
+
+func main() {
+	ui.MostrarMenuPrinc()
+}
---
+++ api/src/ui/ui.go
@@ -0,0 +1,8 @@
+package ui
+
+import "fmt"
+
+func MostrarMenuPrinc() {
+	fmt.Println("Bem-vindo ao sistema de gerenciamento de pedal!\n1 - Cadastrar pedal\n2 - Buscar Pedal\n3 - Voltar")
+
+}
